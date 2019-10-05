b866932e5b7f6f05f7de71f576f82a9ba2f4a241
lockandlol <tks_post@hotmail.com>
2019-10-05 22:19:31 +0200

    window and thread created

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -1,6 +1,76 @@
 package com.company;
 
-public class Game {
+import java.awt.*;
 
+public class Game extends Canvas implements Runnable {
 
+    private boolean isRunning = false;
+    private Thread thread;
+    private int width,height;
+
+    public Game(){
+
+        new Window(1000,563,"Game",this);
+    }
+
+    public Game(int width,int height, String title){
+        this.width = width;
+        this.height = height;
+
+        new Window(width,height,title,this);
+    }
+
+    public void tick(){
+
+    }
+
+    public void render(){
+
+    }
+
+    @Override
+    public void run() {
+        this.requestFocus();
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60;
+        double ns = 1000000000/amountOfTicks;
+        double delta = 0;
+        long timer = System.currentTimeMillis();
+        int frames = 0;
+
+        while(isRunning){
+            long now = System.nanoTime();
+            delta += (now - lastTime)/ns;
+            lastTime = now ;
+            while(delta >= 1){
+                tick();
+                delta--;
+
+            }
+            render();
+            frames++;
+
+            if(System.currentTimeMillis() - timer > 1000){
+                timer += 1000;
+                frames = 0;
+            }
+
+        }
+        stop();
+    }
+
+    public void stop(){
+        isRunning = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void start(){
+        isRunning = true;
+        thread = new Thread(this);
+        thread.start();
+    }
 }
--- src/com/company/Window.java
+++ src/com/company/Window.java
@@ -1,5 +1,25 @@
 package com.company;
 
+import javax.swing.*;
+import java.awt.*;
+
 public class Window {
 
+    public Window(int width, int height, String title, Game game){
+
+        JFrame frame = new JFrame(title);
+
+        Dimension dim = new Dimension(width, height);
+        frame.setPreferredSize(dim);
+        frame.setMaximumSize(dim);
+        frame.setMinimumSize(dim);
+
+        frame.add(game);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+
+        frame.setVisible(true);
+
+    }
 }
